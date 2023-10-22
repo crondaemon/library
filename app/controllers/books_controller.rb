7c210483248aad0249086cd85b3aa82c9d039094
Dario Lombardo <lomato@gmail.com>
2023-10-22 22:09:39 +0200

    chapter 7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     if @book.save
       respond_to do |format|
         format.html{ redirect_to books_path, notice: "Book was successfully created." }
-        format.turbo_stream
+        format.turbo_stream{ flash.now[:notice] = "Book was successfully created." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -30,7 +30,10 @@ class BooksController < ApplicationController
 
   def update
     if @book.update(book_params)
-      redirect_to books_path, notice: "Book was successfully updated."
+      respond_to do |format|
+        format.html{ redirect_to books_path, notice: "Book was successfully updated." }
+        format.turbo_stream{ flash.now[:notice] = "Book was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,7 +44,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html{ redirect_to books_path, notice: "Book was successfully destroyed." }
-      format.turbo_stream
+      format.turbo_stream{ flash.now[:notice] = "Book was successfully destroyed." }
     end
   end
 
